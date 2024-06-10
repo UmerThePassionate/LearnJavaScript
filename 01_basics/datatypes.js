2da532dd50f2f59f2c3dbcbe6273966b93358c84
UmerThePassionate <umersaeed0234567890@gmail.com>
2024-06-10 11:24:51 +0500

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3); we are using nodejs not browser 
+
+// Datatypes
+
+// string
+let name = "umer";
+// number
+let age = 18;
+// boolean
+let isLoggedIn = false;
+// null
+let accountInfo = null;
+// undefined
+let accountId = "";
+
+
+// number => 2 to power 53
+// bigint
+// string  => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
