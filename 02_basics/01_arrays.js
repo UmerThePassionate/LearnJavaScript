07ba98e37225279a2cabf93aed1656ef1a58f314
MuhammadArslan934557 <umersaeed0234567890@gmail.com>
2024-06-27 17:35:28 +0500

    array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -8,5 +8,33 @@ const myArr2 = new Array (1, 2, 3, 4);
 
 // Array methods
 
-myArr.push(3);
-console.log(myArr);
+// myArr.push(3);
+// myArr.push(58);
+// myArr.pop();
+
+// myArr.unshift(9);
+// myArr.shift();
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+// console.log(myArr.indexOf(2));
+
+// const newArr = myArr.join();
+
+// console.log(myArr);
+// console.log(newArr);
+
+// slice, splice
+
+
+// console.log("A", myArr);
+
+// const myn1 = myArr.slice(1, 3);
+
+// console.log(myn1);
+// console.log("B", myArr);
+
+// const myn2 = myArr.splice(1, 3);
+// console.log(myn2);
+// console.log("C", myArr);
+
